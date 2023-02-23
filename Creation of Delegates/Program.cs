7159a5d10d15cfdee7353c47a05e1d311e12b38f
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2023-02-23 14:23:39 +0200

    Add Creation of Delegates

---
+++ Creation of Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Creation_of_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>()
+            {
+                "Hello World :)",
+                "Goodbye World :("
+            };
+
+            Func<string, int> stringLengthDelegate = s => s.Length;
+
+            foreach (string line in lines)
+            {
+                int length = stringLengthDelegate(line);
+                Console.WriteLine(line);
+                Console.WriteLine($"Довжина рядка: {length}.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
